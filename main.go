9226f7523bed52a5c5b4a5b84e23f6b4154205da
AumSahayata <aumsahayata9@gmail.com>
2025-06-03 17:07:13 +0530

    completed makeRequest and isSitemap function

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"strings"
+	"math/rand"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type SEOData struct {
@@ -13,7 +19,7 @@ type SEOData struct {
 	StatusCode		int
 }
 
-type parser interface {
+type Parser interface {
 	
 }
 
@@ -21,14 +27,49 @@ type DefaultParser struct {
 
 }
 
-// userAgents
-
-func randomUserAgent() {
-
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3.1 Safari/605.1.15",
+	"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36>",
+	"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+	"Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-T550 Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.3 Chrome/38.0.2125.102 Safari/537.36",
+	"Mozilla/5.0 (Linux; Android 7.0; SM-T827R4 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Safari/537.36",
 }
 
-func makeRequest() {
+func randomUserAgent() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	randNum := r.Int() % len(userAgents)
+	return userAgents[randNum]
+}
 
+func isSitemap(urls []string) ([]string, []string) {
+	sitemapFiles := []string{}
+	pages := []string{}
+	for _, page := range urls {
+		if foundSitemap == true {
+			fmt.Sprintf("Found sitemap", page)
+			sitemapFiles = append(sitemapFiles, page)
+		} else {
+			pages = append(pages, page)
+		}
+	}
+	return sitemapFiles, pages
+}
+
+func makeRequest(targetURL string) (*http.Response, error){
+	client := http.Client{
+		Timeout: 10*time.Second,
+	}
+	req, err := http.NewRequest("GET", targetURL, nil)
+	req.Header.Set("User-Agent", randomUserAgent())
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func extractSitemapURLs(startURL string) []string {
@@ -75,8 +116,16 @@ func scrapeURLs() {
 
 }
 
-func scrapePage() {
-
+func scrapePage(url string, parser Parser) (SEOData, error){
+	res, err := crawlPage(url)
+	if err != nil {
+		return SEOData{}, err
+	}
+	data, err := parser.getSEOData(res)
+	if err != nil {
+		return SEOData{}, err
+	}
+	return data, nil
 }
 
 func getSEOData() {
